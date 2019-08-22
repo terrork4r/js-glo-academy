052488727333ca68a08f5a8b233771663c5778e5
terrork4r <terrork4r@gmail.com>
2019-08-23 01:22:43 +0300

    создал новую ветку leson01

---
+++ lesson_01/main.js
@@ -0,0 +1,11 @@
+let money = 1, 
+    mission = 'completed', 
+    income = 2, 
+    addExpenses = 3, 
+    deposit = 4, 
+    period = 6;
+
+alert('hello world, i am a programmer');
+
+console.log('mission: ', mission );
+console.log('income: ', income );
