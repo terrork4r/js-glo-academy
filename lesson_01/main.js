6558a6e6e1faf95582abda9a1d2d852a410d5c9f
terrork4r <terrork4r@gmail.com>
2019-09-05 00:47:13 +0300

    практика

--- lesson_01/main.js
+++ lesson_01/main.js
@@ -106,5 +106,3 @@ console.log(appData.getStatusIncome());
 for (let key in appData) {
   console.log('Наша программа включает в себя данные: ' + key + ' - ' + appData [key]);
 }
-appData.getInfoDeposit();
-console.log(appData.percentDeposit, appData.moneyDeposit, appData.calcSavedMoney());
