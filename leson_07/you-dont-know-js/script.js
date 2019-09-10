3ff601a01690a4a3e09d702096413ac05a321192
terrork4r <terrork4r@gmail.com>
2019-09-10 23:39:40 +0300

    первая часть 7 задания

--- leson_07/you-dont-know-js/script.js
+++ leson_07/you-dont-know-js/script.js
@@ -13,7 +13,6 @@ body.setAttribute('style', 'background-image: url(./image/you-dont-know-js.jpg)'
 
 // Исправил заголовок в 3 книге
 let titleNew = book[4].querySelector('a');
-console.log(titleNew);
 titleNew.innerHTML = 'Книга 3. this и Прототипы Объектов';
 
 // Удалил рекламу
@@ -21,15 +20,24 @@ let delet = document.querySelector('.adv');
 body.removeChild(delet);
 
 // Востановить порядок глав во второй и пятой книге 
-
+    // книга вторая
+ let elem2 = book[0].querySelector('ul').querySelectorAll('li');
+ book[0].querySelector('ul').insertBefore(elem2[3], elem2[6]); 
+ book[0].querySelector('ul').insertBefore(elem2[8], elem2[7]); 
+ book[0].querySelector('ul').insertBefore(elem2[2], elem2[10]); 
+ book[0].querySelector('ul').insertBefore(elem2[5], elem2[7]); 
+ book[0].querySelector('ul').insertBefore(elem2[4], elem2[5]); 
+    // книга пятая
+let elem5 = book[5].querySelector('ul').querySelectorAll('li');
+book[5].querySelector('ul').insertBefore(elem5[9], elem5[3]);
+book[5].querySelector('ul').insertBefore(elem5[2], elem5[6]);  
+book[5].querySelector('ul').insertBefore(elem5[5], elem5[8]);   
 
 // в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
 let newElem = document.createElement('li'),
     elem = book[2].querySelectorAll('li'),
     bookUl = book[2].querySelectorAll('ul');
-
 newElem.textContent = 'Глава 8: За пределами ES6';
-console.log(newElem);
 book[2].querySelector('ul').appendChild(newElem);
-book[2].bookUl.insertBefore(elem[10], elem[9]);
+book[2].querySelector('ul').appendChild(elem[9]);
 
